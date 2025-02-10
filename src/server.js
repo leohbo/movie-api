85eb7c2c50472b37688fb4b0332569eaca8bdf72
Leonardo <leohbo59@gmail.com>
2025-02-10 11:02:47 -0300

    add port

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const express = require("express")
 
 const app = express();
 
-const PORT = 3333;
-app.listen(PORT, () => console.log(`Server is running on Port${PORT}`)); // como se fosse o endereço
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)); // como se fosse o endereço
