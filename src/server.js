d84a58b90d1c10e3c8a69b48609e84bb0c20946d
Leonardo <leohbo59@gmail.com>
2025-02-10 15:42:35 -0300

    config routes

--- src/server.js
+++ src/server.js
@@ -2,5 +2,27 @@ const express = require("express")
 
 const app = express();
 
+
+
+app.get("/message/:id/:user", (request, response) => {
+const{id, user} = request.params
+
+response.send(
+   
+   `
+   ID da mensagem: ${id}.
+   Para o usuário: ${user}.
+   
+   `)
+
+})
+
+app.get("/users", (request, response) => {
+const{page, limit} = request.query;
+
+response.send(`Página: ${page}. Mostrar: ${limit}`)
+
+})
+
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)); // como se fosse o endereço
