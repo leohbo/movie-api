772631ba1924d7dccba4b7146264bbed3bcfb85f
Leonardo <leohbo59@gmail.com>
2025-02-11 16:10:13 -0300

    AppError

---
+++ src/utils/AppError.js
@@ -0,0 +1,14 @@
+class AppError {
+
+  message;
+  statusCode;
+  
+  constructor(message, statusCode = 400) {
+  
+  this.message = message;
+  this.statuscode = statusCode;
+  
+    }
+  }
+  
+  module.exports = AppError;
