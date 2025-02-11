a80a0b3f79a8c283a9c3e6f6d74301374979db16
Leonardo <leohbo59@gmail.com>
2025-02-11 13:26:55 -0300

    config routes

--- src/server.js
+++ src/server.js
@@ -1,17 +1,12 @@
 const express = require('express')
 
+const routes = require("./routes")
+
 const app = express()
 app.use(express.json())
 
-app.post('/users', (request, response) => {
-  
-
-  
-  const { name, email, password } = request.body
-  
-response.json({name, email, password})
-
-})
+app.use(routes);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)); // como se fosse o endereço
+
