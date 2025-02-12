835749fc522be2e603148aa391cbe6ec2f9c5eb6
Leonardo <leohbo59@gmail.com>
2025-02-12 14:31:09 -0300

    install and config sqlite

--- src/server.js
+++ src/server.js
@@ -1,37 +1,35 @@
-require("express-async-errors")
+require('express-async-errors')
 
-const AppError = require("./utils/AppError")
+const database = require("./database/sqlite")
+
+const AppError = require('./utils/AppError')
 
 const express = require('express')
 
-const routes = require("./routes")
+const routes = require('./routes')
 
 const app = express()
-
 app.use(express.json())
 
-app.use(routes);
+app.use(routes)
+
+database();
 
 app.use((error, request, response, next) => {
-if(error instanceof AppError) {
-return response.status(error.statusCode).json({ //erro do cliente
-    status:"error",
-    message: error.message
-});
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
 
-}
+  console.error(error)
 
-console.error(error);
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  })
+})
 
-return response.status.status(500).json({
-     status:"error",
-     message:"Internal Server Error" //erro do servidor
-
-  });
-});
-
-
-
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)); // como se fosse o endereço
-
+const PORT = 3000
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
