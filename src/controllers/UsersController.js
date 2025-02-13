e66b808aa9be9f3d77bf2ef09c689ce7063dce56
Leonardo <leohbo59@gmail.com>
2025-02-13 16:12:43 -0300

    update user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -22,9 +22,56 @@ await database.run(
     [name, email, hashedPassword]
   )
 
+
+
 return response.status(201).json()
 
 }
+
+async update(request, response) {
+const{name, email} = request.body;
+const{id} = request.params;
+
+const database = await sqliteConnection();
+const user = await database.get("SELECT * FROM users WHERE id = (?)",[id]);
+
+if(!user) {
+  throw new AppError("Usuário não encontrado")
+}
+
+const userwithUpdatedEmail = await database.get("SELECT FROM * users WHERE id email = (?)", [email])
+
+if(userwithUpdatedEmail && userwithUpdatedEmail.id !== user.id) {
+  throw new AppError("Este e-mail já segue em uso")
+
+}
+
+user.name = name;
+user.email = email;
+
+await database.run(`
+  UPDATE users SET
+  name = ?,
+  email = ?,
+  password = ?,
+  updated_at = ?
+  WHERE id = ?`,[user.name, user.email, update_at = DATETIME('now'), id])
+
+return response.json();
+
+
+/*
+if(password && old_password) {
+throw new AppError("É necessário informar a senha antiga")
+
+
+  }
+  
+  const checkOldPassword = await compare(old_password, user.password) 
+ 
+*/
+}
  }
+
 
 module.exports = UsersController;
