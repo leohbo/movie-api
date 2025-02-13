d2dad905b2c490f175fb343295b123ebb52e8002
Leonardo <leohbo59@gmail.com>
2025-02-13 16:12:59 -0300

    add userRoutes PUT

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,6 +8,7 @@ const usersRoutes = Router();
 const usersController = new UsersController();
 
 usersRoutes.post('/', usersController.create);
+usersRoutes.put('/:id', usersController.update);
 
 
 module.exports = usersRoutes;
