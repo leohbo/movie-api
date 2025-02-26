4d55966564404e0de38f5aa4c77149e3361a31b9
Leonardo <leohbo59@gmail.com>
2025-02-26 16:33:53 -0300

    add show and delete

---
+++ src/controllers/NotesController.js
@@ -0,0 +1,57 @@
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+
+class NotesController {
+   async create(request, response) {
+   const {title, description, rating, tags} = request.body;
+   const {user_id} = request.params;
+
+if(rating < 0 || rating > 5) {
+   throw new AppError("A nota deve ser entre 0 e 5")
+}
+
+const [note_id] = await knex("movie_notes").insert({
+title,
+description,
+rating,
+user_id
+
+});
+
+const tagsInsert = tags.map(name => {
+  return {
+  note_id,
+  user_id,
+  name
+}
+});
+
+await knex("movie_tags").insert(tagsInsert);
+
+return response.json();
+
+  }
+
+  async show(request, response) {
+   const { id } = request.params;
+
+   const note = await knex("movie_notes").where({ id }).first();
+   const tags = await knex("movie_tags").where({ note_id: id }).orderBy("name");
+
+   return response.json({
+       ...note,
+       tags
+   });
+} 
+
+  async delete(request, response) {
+  const{id} = request.params
+  await knex("movie_notes").where({id}).delete()
+  
+  return response.json()
+
+}
+
+}
+
+module.exports = NotesController;
